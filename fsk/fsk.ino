a1069950c8dde638c42f6fb12d78e70dff3af668
David DeLong <limerickcabin@hotmail.com>
2023-12-14 17:11:54 -0600

    Update fsk.ino

--- fsk/fsk.ino
+++ fsk/fsk.ino
@@ -1,4 +1,5 @@
 /*
+ * Sends AIS messages via Heltec WiFi LoRa module
 */
 
 #include "Arduino.h"
@@ -15,15 +16,14 @@
 SX1262 radio = new Module(8,14,12,13);
 
 //bit-stuffed hdlc packet in AIS packet bit order (bit 0 is on the left)
-//bits in each byte are swapped in nrzi() so that the first bit sent is the lsb of the AIS packet (left to right)
-byte packet[]={0xaa,0xaa,0xaa,0x7e,0x4,0x57,0x9e,0x5b,0xe1,0x60,0x00,0x06,0x1d,0x0f,
-               0x30,0x0b,0x9f,0x1b,0x1d,0x20,0x00,0xf0,0x00,0x80,0x6b,0x1c,0xe1,0xfd,0xfe};
+//bits in each byte are swapped in nrzi() because RadioLib sends left to right (msb of byte 0 first)
+byte packet[]={0xaa,0xaa,0xaa,0x7e,0x04,0x57,0x9e,0x5b,0xe1,0x60,0x00,0x06,0x1d,0x0f,
+               0x30,0x0b,0x9f,0x1b,0x1d,0x20,0x00,0xf0,0x00,0x80,0x6b,0x1c,0xe1,0xfd,0xfe}; //don't forget the last 0
 byte nrziPacket[29];
 
-#define BAND 433e6
 void setup()
 {
-	Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/, true /*LoRa use PABOOST*/, BAND /*LoRa RF working band*/);
+	Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
 
 	logo();
 	delay(3000);
@@ -31,25 +31,24 @@ void setup()
 
   radioLibSetup();
   nrzi(packet,sizeof(packet),nrziPacket);
-  Heltec.display -> drawString(0, 20, "Starting AIS transmissions");
-  Heltec.display -> display();
 }
 
 void loop()
 {
+  static uint32_t count;
+  static char buf[80];
+
   // transmit FSK packet
-  int len=sizeof(nrziPacket);
-  int state = radio.transmit(nrziPacket,len);
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("[SX1262] Packet transmitted successfully!"));
-  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
-    Serial.println(F("[SX1262] Packet too long!"));
-  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
-    Serial.println(F("[SX1262] Timed out while transmitting!"));
-  } else {
-    Serial.println(F("[SX1262] Failed to transmit packet, code "));
-    Serial.println(state);
-  }
+  int state = radio.transmit(nrziPacket,sizeof(nrziPacket));
+  if (state == RADIOLIB_ERR_NONE) Serial.println(F("[SX1262] Packet transmitted successfully!"));
+  else Serial.println(state);
+  
+  sprintf(buf,"count %d",count++);
+	Heltec.display -> clear();
+  Heltec.display -> drawString(0, 20, "Sending AIS transmissions");
+  Heltec.display -> drawString(0, 30, buf);
+  Heltec.display -> display();
+
   delay(10000);
  }
 
@@ -76,13 +75,13 @@ void radioLibSetup() {
   state = radio.setFrequency(162.025);
   state = radio.setBitRate(9.6);
   state = radio.setFrequencyDeviation(2.4);
-  state = radio.setRxBandwidth(250.0);
-  state = radio.setOutputPower(20.0);
+  state = radio.setRxBandwidth(58.6);
+  state = radio.setOutputPower(-9.0);
   state = radio.setCurrentLimit(100.0);
   state = radio.setDataShaping(RADIOLIB_SHAPING_1_0);
   state = radio.setCRC(0);
   state = radio.setPreambleLength(1);
-  state = radio.setSyncWord((uint8_t)0xcc, 8);
+  state = radio.setSyncWord((uint8_t)0xcc, 8); //seems not to like this
   if (state != RADIOLIB_ERR_NONE) {
     Serial.print(F("Unable to set configuration, code "));
     Serial.println(state);
